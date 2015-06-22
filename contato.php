34520fd0168151a727b0da69de65f32eb19ff0f4
Newton Arruda <newton.arruda@gmail.com>
2015-06-22 18:05:44 -0300

    novas imagens

--- contato.php
+++ contato.php
@@ -27,14 +27,14 @@
 		if ($human !== 5) {
 			$errHuman = 'Your anti-spam is incorrect';
 		}
-// If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
-	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
-	}
-}
+		// If there are no errors, send the email
+		if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+			if (mail ($to, $subject, $body, $from)) {
+				$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+			} else {
+				$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
+			}
+		}
 	}
 ?>
 
