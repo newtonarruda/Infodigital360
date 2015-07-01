ac765aa9daa68b9cf1f453f9bbbe85db744c06a5
Newton Arruda <newton.arruda@gmail.com>
2015-07-01 19:50:29 -0300

    formulario de contato em php

--- contato.php
+++ contato.php
@@ -1,39 +1,119 @@
 <?php
-	if ($_POST["submit"]) {
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
-		$human = intval($_POST['human']);
-		$from = 'HotSite360'; 
-		$to = 'newton.arruda@gmail.com'; 
-		$subject = 'Message from Contact Demo ';
-		
-		$body ="From: $name\n E-Mail: $email\n Message:\n $message";
-		// Check if name has been entered
-		if (!$_POST['name']) {
-			$errName = 'Please enter your name';
-		}
-		
-		// Check if email has been entered and is valid
-		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$errEmail = 'Please enter a valid email address';
-		}
-		
-		//Check if message has been entered
-		if (!$_POST['message']) {
-			$errMessage = 'Please enter your message';
-		}
-		//Check if simple anti-bot test is correct
-		if ($human !== 5) {
-			$errHuman = 'Your anti-spam is incorrect';
-		}
-		// If there are no errors, send the email
-		if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
-			if (mail ($to, $subject, $body, $from)) {
-				$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-			} else {
-				$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
-			}
-		}
-	}
-?>
+$subjectPrefix = '[Contato via Site]';
+$emailTo = 'newton.arruda@gmail.com';
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name    = stripslashes(trim($_POST['form-name']));
+    $email   = stripslashes(trim($_POST['form-email']));
+    $subject = stripslashes(trim($_POST['form-subject']));
+    $message = stripslashes(trim($_POST['form-message']));
+    $pattern  = '/[\r\n]|Content-Type:|Bcc:|Cc:/i';
+    if (preg_match($pattern, $name) || preg_match($pattern, $email) || preg_match($pattern, $subject)) {
+        die("Header injection detected");
+    }
+    $emailIsValid = preg_match('/^[^0-9][A-z0-9._%+-]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/', $email);
+    if($name && $email && $emailIsValid && $subject && $message){
+        $subject = "$subjectPrefix $subject";
+        $body = "Nome: $name <br /> Email: $email <br /> Mensagem: $message";
+        $headers  = 'MIME-Version: 1.1' . PHP_EOL;
+        $headers .= 'Content-type: text/html; charset=utf-8' . PHP_EOL;
+        $headers .= "From: $name <$email>" . PHP_EOL;
+        $headers .= "Return-Path: $emailTo" . PHP_EOL;
+        $headers .= "Reply-To: $email" . PHP_EOL;
+        $headers .= "X-Mailer: PHP/". phpversion() . PHP_EOL;
+        mail($emailTo, $subject, $body, $headers);
+        $emailSent = true;
+    } else {
+        $hasError = true;
+    }
+}
+?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, height=device-height, maximum-scale=1, user-scalable=no">
+
+    <title>Infodigital GBV</title>
+	    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div class="jumbotron">
+        <h1>Simple PHP Contact Form</h1>
+        <p>A Simple Contact Form developed in PHP with VanillaJS Form validation.</p>
+    </div>
+    <?php if(!empty($emailSent)): ?>
+        <div class="col-md-6 col-md-offset-3">
+            <div class="alert alert-success text-center">Sua mensagem foi enviada com sucesso.</div>
+        </div>
+    <?php else: ?>
+        <?php if(!empty($hasError)): ?>
+        <div class="col-md-5 col-md-offset-4">
+            <div class="alert alert-danger text-center">Houve um erro no envio, tente novamente mais tarde.</div>
+        </div>
+        <?php endif; ?>
+
+    <div class="col-md-6 col-md-offset-3">
+        <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" id="contact-form" class="form-horizontal" role="form" method="post">
+            <div class="form-group">
+                <label for="name" class="col-lg-2 control-label">Nome</label>
+                <div class="col-lg-10">
+                    <input type="text" class="form-control required" id="form-name" name="form-name" placeholder="Nome" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="email" class="col-lg-2 control-label">Email</label>
+                <div class="col-lg-10">
+                    <input type="email" class="form-control required" id="form-email" name="form-email" placeholder="Email" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="assunto" class="col-lg-2 control-label">Assunto</label>
+                <div class="col-lg-10">
+                    <input type="text" class="form-control required" id="form-subject" name="form-subject" placeholder="Assunto" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="mensagem" class="col-lg-2 control-label">Mensagem</label>
+                <div class="col-lg-10">
+                    <textarea class="form-control required" rows="3" id="form-message" name="form-message" placeholder="Mensagem" /></textarea>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-lg-offset-2 col-lg-10">
+                    <button type="submit" class="btn btn-default">Enviar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <?php endif; ?>
+
+	    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+<!--    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>-->
+<!--    <script src="js/classie.js"></script>-->
+<!--    <script src="js/cbpAnimatedHeader.js"></script>-->
+
+    <!-- Contact Form JavaScript -->
+<!--    <script src="js/jqBootstrapValidation.js"></script>-->
+<!--    <script src="js/contact_me.js"></script>-->
+
+    <!-- Custom Theme JavaScript -->
+    <script type="text/javascript" src="js/index.js"></script>
+
+</body>
+</html>
