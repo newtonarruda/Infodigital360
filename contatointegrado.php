a0a034926c9f0c514b4ce0d0cefc5c4472b6ebee
Newton Arruda <newton.arruda@gmail.com>
2015-07-05 15:32:55 -0300

    formulario contato

--- contatointegrado.php
+++ contatointegrado.php
@@ -1,6 +1,6 @@
 <?php
-$subjectPrefix = '[Contato via Site]';
-$emailTo = 'newton.arruda@gmail.com';
+$subjectPrefix = '[Contato via Infodigital360.com.br]';
+$emailTo = 'vendas@infodigital.com.br';
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name    = stripslashes(trim($_POST['form-name']));
     $email   = stripslashes(trim($_POST['form-email']));
